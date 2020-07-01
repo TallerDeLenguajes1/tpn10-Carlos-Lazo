5f9facbcbf6be07ee44663f00077bb29bbcff78c
Carlos-Lazo <carlos.lazo550@gmail.com>
2020-07-01 17:29:54 -0300

    Punto 1

---
+++ TP10/Helper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helper
+{
+    class Helpers
+    {
+
+    }
+    public class Inmobiliaira {
+        enum TipoDeOperacion
+        {
+            Venta= 0,
+            Alquiler=1,
+        }
+        enum TipoDePropiedad
+        {
+            Depto = 0,
+            Casa = 1,
+            Duplex=2,
+            PenthHouse=3,
+            Terreno=4,
+
+        }
+
+        int id;
+        float tamanio;
+        int cantidadDeBanios;
+        int cantidadDeCuartos;
+        string domicilio;
+        int precio;
+        bool estado;
+        double valorMueble;
+
+        public int Id { get => id; set => id = value; }
+        public float Tamanio { get => tamanio; set => tamanio = value; }
+        public int CantidadDeBanios { get => cantidadDeBanios; set => cantidadDeBanios = value; }
+        public int CantidadDeCuartos { get => cantidadDeCuartos; set => cantidadDeCuartos = value; }
+        public string Domicilio { get => domicilio; set => domicilio = value; }
+        public int Precio { get => precio; set => precio = value; }
+        public bool Estado { get => estado; set => estado = value; }
+        public double ValorMueble { get => valorMueble; set => valorMueble = value; }
+
+        public double ValorDelInmueble(int Operacion, int precio)
+        {
+            double valor = 0;
+            if (Operacion == 0)
+            {
+                valor = (precio * 1.31) ;
+                valor += 10000; 
+            }else if(Operacion == 1)
+            {
+                valor = (precio * 1.025) ;
+
+            }
+            else
+            {
+                valor = -9999;
+            }
+
+            return  valor;
+        }
+
+        public void CargarDatos(int _id, float _tama, int _cantBanios, int _cantCuartos, string _Domicilio, int _Precio, bool _Estado, int _Operacion)
+        {
+            Id = _id;
+            Tamanio = _tama;
+            CantidadDeBanios = _cantBanios;
+            CantidadDeCuartos = _cantBanios;
+            Domicilio = _Domicilio;
+            Precio = _Precio;
+            ValorMueble = ValorDelInmueble(_Operacion,_Precio);
+
+        } 
+    
+    }
+
+
+}
---
+++ TP10/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using Helper;
+using System.Collections.Generic;
+
+namespace TP10
+{
+    class Principal
+    {
+        static void Main(string[] args)
+        {
+            string NombreArchivo = "Ventas.txt";
+            float[] tama = {28.9F , 50.3F, 90.8F };
+            int[] Banios = { 1, 2, 3, 4 };
+            int[] Dormitorios = { 1, 2, 3, 4 };
+            string[] Domicilio = { "Bº Sur", "Bº Norte", "Bº Centro" };
+            int[] Precio = { 1000000, 150000, 2000000, 2500000, 300000 };
+            bool Estado = false;
+
+            int CantidadDClientes = 2; ///new Random().Next(11);
+
+            List<Inmobiliaira> ListaVentas = new List<Inmobiliaira>();
+
+            Inmobiliaira propieda;
+
+            for (int i = 0; i < CantidadDClientes ; i++)
+            {
+                propieda = new Inmobiliaira();
+                if(i % 2 == 0) { Estado = true; };
+                propieda.CargarDatos(i, tama[new Random().Next(tama.Length)], Banios[new Random().Next(Banios.Length)], Dormitorios[new Random().Next(Dormitorios.Length)], Domicilio[new Random().Next(Domicilio.Length)], Precio[new Random().Next(Precio.Length)], Estado, new Random().Next(2));
+                Console.WriteLine(propieda.Id + propieda.Tamanio);
+                ListaVentas.Add(propieda);
+            }
+
+            using (StreamWriter StrW = new StreamWriter(NombreArchivo))
+            {
+
+
+                foreach (Inmobiliaira Venta in ListaVentas)
+                {
+                    string articulo = Convert.ToString(Venta.Id) + ";" + Convert.ToString(Venta.Tamanio) + ";" + Convert.ToString(Venta.CantidadDeBanios) + ";" + Convert.ToString(Venta.CantidadDeCuartos) + ";" + Venta.Domicilio + ";" + Convert.ToString(Venta.Precio) + ";" + Convert.ToString(Venta.ValorMueble) + "\n";
+                    //Console.WriteLine(articulo);
+                    StrW.WriteLine(articulo);
+                }
+
+            }
+        }
+    }
+}
